4d61e09d3e70ab31b020c5d1cadacd2a03524cbc
Robert Kuhlmann <robert.kuhlmann2@gmail.com>
2018-10-28 22:32:27 +0200

    projects page and carousel

--- src/components/projectlist.js
+++ src/components/projectlist.js
@@ -1,21 +1,114 @@
 import React from 'react';
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+import { getImageLinks } from '../projectData';
 
 const ListWrapper = styled.div`
-    
+    padding: 50px 0;
 `;
 
 const ProjectWrapper = styled.article`
-
+    box-shadow: 0 0 5px var(--dark);
+    padding: 80px 100px;
+    margin-bottom: 30px;
 `;
+
+const SliderImage = styled.img`
+    display: inline-block;
+    width: 100%;
+`;
+
+const ImageSlider = ({image}) => {
+
+    const images = getImageLinks();
+    console.log(image);
+
+    if(typeof image === 'null') return null;
+
+    if(image.constructor === Array) {
+        const settings = {
+            infinite: true,
+            speed: 1000,
+            autoplaySpeed: 5000,
+            autoplay: true,
+            dots: true
+        }
+        return (
+            <div>
+                <Slider {...settings}>
+                    {image.map(
+                        (img, i) => (
+                            <div key={i}>
+                                <SliderImage 
+                                    src={images[img]}
+                                    alt={img}></SliderImage>
+                            </div>
+                        )
+                    )}
+                </Slider>
+            </div>
+        );
+    }
+
+    if(typeof image === 'string') {
+        return (
+            <div>
+                <SliderImage 
+                    src={images[image]}
+                    alt={image}></SliderImage>
+            </div>
+        );
+    }
+
+    return null;
+}
+
+
+
+const Taglist = styled.div`
+    display: flex; 
+    flex-wrap: wrap;
+
+    span {
+        display: inline-block;
+        margin: 3px;
+        background-color: var(--adjacent1);
+        padding: 5px 7px;
+        border-radius: 10px;
+        color: white;
+        font-size: 14px;
+    }
+`;
+
+const titleStyle = {
+    color: 'var(--primary)',
+    margin: '0 0 10px'
+}
+
+const subTitleStyle = {
+    color: 'var(--dark)',
+    margin: '15px 0'
+};
 
 const ProjectList = ({projects}) => (
     <ListWrapper>
         {projects.map((project, i) => (
             <ProjectWrapper key={i}>
-                <h2>{project.name}</h2>
-                <h4>{project.type}</h4>
-                
+                <h1 style={titleStyle}>{project.name}</h1>
+                <h3 style={subTitleStyle}>{project.type}</h3>
+                <Taglist>
+                    {project.tags.map((tag, i) => (<span key={i}>{tag}</span>))}
+                </Taglist>
+                <p>{project.text}</p>
+                <div>
+                    <ImageSlider image={project.image}></ImageSlider>
+                </div>
             </ProjectWrapper>
         ))}
     </ListWrapper>
--- src/projectData.js
+++ src/projectData.js
@@ -4,8 +4,8 @@ export const projectData = [
         type: 'Kouluprojekti',
         tags: ['jQuery', 'Google Maps API', 'Google Street View', 'Bootstrap'],
         text: 'Kouluprojektina ryhmässä toteuttamamme "seikkailupeli", jossa seikkaillaan Kolin kansallismaisemissa. Tavoitteena projektissa oli pyrkiä pelillistämään matkailua jollain tapaa. Opin projektissa paljon etenkin Google Maps API:n käyttöä.',
-        published: new Date(2016, 10, 1, 0, 0, 0, 0),
-        image: '',
+        published: new Date(2016, 9, 1, 0, 0, 0, 0),
+        image: ['virtualnature.png', 'virtualnature2.png'],
         github: 'https://github.com/roopertti/Virtuaaliluonto',
         site: null
     },
@@ -15,7 +15,7 @@ export const projectData = [
         tags: ['Node.js', 'Express.js','Angular 1.x (vanha)', 'Bootstrap'],
         text: 'Tyypillinen korttipakalla pelattava bilepeli luotuna digitaaliseen formaattiin. Projekti ei koskaan ihan valmistunut, mutta opetti paljon front-end puolen kehittämistä. Nykyään tosin Angular 1.x on hyvin pitkälti käytöstä jo poistunut, mutta ajoi asiansa tuolloin.',
         published: new Date(2017, 1, 1, 0, 0, 0, 0),
-        image: '',
+        image: 'partypeli.png',
         github: null,
         site: null
     },
@@ -24,8 +24,8 @@ export const projectData = [
         type: 'Oma projekti',
         tags: ['Node.js', 'Express.js', 'Socket.io', 'jQuery', 'Bootstrap'],
         text: 'Ensimmäinen kokeiluni Socket.io kirjaston kanssa. Toteutin verkon yli toimivan chatin, jossa keskustelu tallentui palvelimen sen hetkiseen välimuistiin. Opettavainen kokemus sen suhteen, miten erilaisia tapahtumia tulee käsitellä palvelimen puolella.',
-        published: new Date(2016, 8, 1, 0, 0, 0, 0),
-        image: '',
+        published: new Date(2016, 10, 1, 0, 0, 0, 0),
+        image: 'chatapp.png',
         github: 'https://github.com/roopertti/ChatApp',
         site: null
     },
@@ -35,7 +35,7 @@ export const projectData = [
         tags: ['Node.js', 'Express.js', 'Angular', 'Google Firebase', 'MaterialUI'],
         text: 'Suhteellisen tuore projekti, joka toteutettiin osana koulukurssia. Työskentelin pääosin projektissamme itsenäisesti front-endin parissa. Minulle tuli hyvin tutuksi uudistuneen Angularin toiminnot, sekä TypeScript-laajennus. Myös Googlen suosittu MaterialUI-kirjasto tuli tutuksi.',
         published: new Date(2018, 5, 1, 0, 0, 0, 0),
-        image: '',
+        image: ['kiinteisto1.png', 'kiinteisto2.png'],
         github: null,
         site: null
     },
@@ -45,7 +45,7 @@ export const projectData = [
         tags: ['Node.js', 'Express.js', 'MongoDB', 'Angular 1.x (vanha)', 'Bootstrap'],
         text: 'Kesätöikseni toisen opiskelijan kanssa kehittelemäni ajanvarausjärjestelmä, jonka avulla pystyttiin helpottamaan ajanvarausprosessia opintokuraattorille.',
         published: new Date(2017, 5, 1, 0, 0, 0, 0),
-        image: '',
+        image: ['kuraattori.png', 'kuraattori2.png'],
         github: null,
         site: 'https://web.karelia.fi/opintokuraattorin-ajanvaraus/'
     },
@@ -55,7 +55,7 @@ export const projectData = [
         tags: ['React', 'Redux', 'Google Firebase', 'Google Cloud Functions','Jest', 'Enzyme', 'Styled Components'],
         text: 'Opinnäytetyötäni varten toteutuksen alla olevan opiskelijatapahtumakalenteri. Kalenteri tehdään yhden sivun sovelluksena (SPA) käyttämällä nykyään hyvin suosittuja React- ja Redux-kirjastoja. Ominaisuudet pyrtään myös yksikkötestaamaan huolellisesti Jest- ja Enzyme-työkalujen avulla.',
         published: new Date(2018, 10, 1, 0, 0, 0, 0),
-        image: '',
+        image: ['oppari1.png', 'oppari2.png'],
         github: null,
         site: 'https://student-events.firebaseapp.com/'
     },
@@ -63,9 +63,9 @@ export const projectData = [
         name: 'Tämä portfolio sivu!',
         type: 'Oma projekti',
         tags: ['React', 'Gatsby', 'Styled Components'],
-        text: 'Tässä yhteen viikonloppuun kiireellä kasaan paiskittu perus staattisesta sisällöstä koostuva sivusto, tavoitteena kokeilla Gatsby-työkalun ominaisuuksia yhdessä React:in kanssa. Toivottavasti portfolio on asiansa ajanut tähän asti!',
+        text: 'Tässä yhteen viikonloppuun nopeasti kasaan paiskittu perus staattisesta sisällöstä koostuva sivusto, tavoitteena kokeilla Gatsby-työkalun ominaisuuksia yhdessä React:in kanssa. Toivottavasti portfolio on asiansa ajanut tähän asti!',
         published: new Date(2018, 10, 1, 0, 0, 0, 0),
-        image: '',
+        image: 'portfolio.png',
         github: '',
         site: ''
     }
@@ -74,3 +74,17 @@ export const projectData = [
 export const returnProjectsSorted = () => (projectData.sort((a, b) => {
     return a.published.getTime() - b.published.getTime();
 }));
+
+export const getImageLinks = () => {
+    const imageFiles = require.context('./images/projektikuvat', false, /\.(png|jpe?g|svg)$/);
+    const keys = imageFiles.keys();
+    const imageSources = keys.map(imageFiles);
+    let images = {};
+    for(const index of keys.keys()) {
+        const key = keys[index].substring(2);
+
+        images = { ...images, [key]: imageSources[index] };
+    }
+
+    return images;
+}
